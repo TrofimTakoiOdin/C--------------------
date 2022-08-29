e24622caf6747d9fb7dfefe657075e276573a2d4
arseny <arseniy.trofimov1993@gmail.com>
2022-08-29 11:08:15 +0300

    int division

--- Examples GB/Example_004_division/Program.cs
+++ Examples GB/Example_004_division/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Целочисленное деление
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Частное равно: " + a / b);
+    }
+}
+
+
