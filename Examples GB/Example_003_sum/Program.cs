55f9df43c1c123aaa7d146d822a1759f2cc87d6e
arseny <arseniy.trofimov1993@gmail.com>
2022-08-29 10:54:40 +0300

    Added sum

---
+++ Examples GB/Example_003_sum/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Сумма равна: " + (a + b));
+    }
+}
