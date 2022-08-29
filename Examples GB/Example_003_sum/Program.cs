4b20823652c4f40357a26d0a7b1e2179f154d003
arseny <arseniy.trofimov1993@gmail.com>
2022-08-29 16:51:33 +0300

    added random

--- Examples GB/Example_003_sum/Program.cs
+++ Examples GB/Example_003_sum/Program.cs
@@ -2,10 +2,10 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Введите число: ");
-        int a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Введите число: ");
-        int b = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Сумма равна: " + (a + b));
+        int a = new Random().Next(1, 11);
+        Console.WriteLine("Первое число: " + a + " ");
+        int b = new Random().Next(1, 11);
+        Console.WriteLine("Второе число: " + b);
+        Console.WriteLine("Сумма первого и второго чисел равна: " + (a + b));
     }
 }
