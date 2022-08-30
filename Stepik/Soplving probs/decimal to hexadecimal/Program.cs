d4c4957e71c720920947f709de7a6c1fbe43ba62
arseny <arseniy.trofimov1993@gmail.com>
2022-08-30 12:04:08 +0300

    decimal to hexadecimal

---
+++ Stepik/Soplving probs/decimal to hexadecimal/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        string line = Console.ReadLine(); // ввод числа в десятеричной системе 
+
+        int x = int.Parse(line);
+
+        string answer = Convert.ToString(x, 16);
+        
+
+        Console.WriteLine(answer);
+    }
+}
