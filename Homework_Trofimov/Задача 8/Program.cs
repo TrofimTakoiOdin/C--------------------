faac23deeba75de783b4e5ace6c6cced0cadc1fd
arseny <arseniy.trofimov1993@gmail.com>
2022-08-31 17:49:56 +0300

    Solved problem 8

---
+++ Homework_Trofimov/Задача 8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine($"Введите целое число большее {1}:");
+string a = Console.ReadLine();
+int b = Convert.ToInt32(a);
+if (b < 2)
+{
+    Console.WriteLine($"Укажите число большее {1} и попробуйте снова");
+}
+else
+{
+    for (int i = 2; i <= b; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write($"{i} ");
+        }
+    }
+}
