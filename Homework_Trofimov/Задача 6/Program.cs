6d080a462f36c7978d9dc7c32820ee25670e7bbc
arseny <arseniy.trofimov1993@gmail.com>
2022-08-31 17:03:16 +0300

    Solved problem 6

---
+++ Homework_Trofimov/Задача 6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Давайте проверим, четное ли число?");
+Console.WriteLine("Введите целое число: ");
+string a = Console.ReadLine();
+int b = Convert.ToInt32(a);
+if (b % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
