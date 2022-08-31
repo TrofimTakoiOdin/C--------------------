ddb81f2dbebc1665396cc28849abe8357230e298
arseny <arseniy.trofimov1993@gmail.com>
2022-08-31 13:36:44 +0300

    solved prob

--- Stepik/IFs/Program.cs
+++ Stepik/IFs/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {   
+        string line = Console.ReadLine();
+        string[] splitString = line.Split(' ');
+        int[] numbers = new int[3];
+        numbers = splitString.Select(x => int.Parse(x)).ToArray();
+        int minn = numbers.Min();
+        Console.WriteLine(minn);
+    }
+}
