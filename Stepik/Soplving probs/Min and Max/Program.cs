696f771557b7d4a157bf920e469258e4a3cafd44
arseny <arseniy.trofimov1993@gmail.com>
2022-08-31 23:28:18 +0300

    another min and max

---
+++ Stepik/Soplving probs/Min and Max/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите четыре целых числа через пробел в одну строку: ");
+        string line = Console.ReadLine();
+        string[] splitString = line.Split(' ');
+
+        int a = Convert.ToInt32(splitString[0]);
+        int b = Convert.ToInt32(splitString[1]);
+        int c = Convert.ToInt32(splitString[2]);
+        int d = Convert.ToInt32(splitString[3]);
+
+        int minValue = a;
+        if (b < minValue)
+        {
+            minValue = b;
+        }
+        if (c < minValue)
+        {
+            minValue = c;
+        }
+        if (d < minValue)
+        {
+            minValue = d;
+        }
+        
+        int maxValue = a;
+
+        if (b > maxValue)
+        {
+            maxValue = b;
+        }
+        if (c > maxValue)
+        {
+            maxValue = c;
+        }
+        if (d > maxValue)
+        {
+            maxValue = d;
+        }
+
+        Console.WriteLine($"Наименьшее число - {minValue}");
+        Console.WriteLine($"Наибольшее число - {maxValue}");
+
+
+        
+
+        
+
+    }
+}
