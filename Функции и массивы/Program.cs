10773e569f179662611f7dcaffaef8b44db85723
arseny <arseniy.trofimov1993@gmail.com>
2022-09-01 12:51:04 +0300

    work in progress

---
+++ Функции и массивы/Program.cs
@@ -0,0 +1,57 @@
+﻿// Функция заполняет массив рандомными целыми числами от 1 до 9
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index ++;
+    }
+}   
+// Функция вывода массива на экран
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    Console.WriteLine("Список элементов: ");
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position ++;
+    }
+}
+// Функция ищет индекс первого вхождения элемента в массив
+int IndexOf(int[] collection, int find) 
+{
+    int count = collection.Length;
+    int index = 0;
+    int position;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        else 
+        {
+            Console.WriteLine("Такого числа нет в массиве")
+        }
+        index ++;
+    }
+    return position;
+}
+// Массив array
+int[] array = new int [10];
+// Призываем функции
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(); // Пустая строка
+// Просим пользователя ввести число, которое будем искать по индексу
+Console.Write("Введите целое число число, индекс которого необходимо найти: ");
+int IntElem = Convert.ToInt32(Console.ReadLine());  // Ввод пользователя
+int pos = IndexOf(array, IntElem);  // Вспомогательная переменная
+// Вывод на экран
+Console.WriteLine($"Искомый элемент: {IntElem}");
+Console.WriteLine($"Позиция элемента в списке: {pos}");
