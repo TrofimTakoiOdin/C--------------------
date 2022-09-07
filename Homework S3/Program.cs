74ba27671c311f8cfde0a627f59075807f7847e5
arseny <arseniy.trofimov1993@gmail.com>
2022-09-07 17:33:50 +0300

    Started working

---
+++ Homework S3/Program.cs
@@ -0,0 +1,29 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        void FiveDigitPalindrome()
+        {
+            Console.WriteLine("Введите пятизначное число: ");
+            string a = new(Console.ReadLine());
+            int x;
+            if (int.TryParse(a, out x) && x > 9999 && x < 100000)
+            {
+                string b = new(string.Join("", a.Reverse()));
+                if (a == b)
+                {
+                    Console.WriteLine("Да, это палиндром");
+                }
+                else
+                {
+                    Console.WriteLine("Нет, это не палиндром!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели что-то не то!");
+            }
+        }
+    }
+    
+}
