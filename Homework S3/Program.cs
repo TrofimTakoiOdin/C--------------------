644e4def66ab877b1b87eebcca85f00e3f56dad1
arseny <arseniy.trofimov1993@gmail.com>
2022-09-08 11:20:53 +0300

    Created some methodes and menu

--- Homework S3/Program.cs
+++ Homework S3/Program.cs
@@ -2,6 +2,11 @@
 {
     private static void Main(string[] args)
     {
+        static void Spaces()
+        {
+            Console.WriteLine();
+            Console.WriteLine("=================");
+        }
         static void FiveDigitPalindrome()
 
         {
@@ -18,6 +23,7 @@
                 else
                 {
                     Console.WriteLine("Нет, это не палиндром!");
+                    
                 }
             }
             else
@@ -42,16 +48,78 @@
         Console.WriteLine($"Координаты точки Б: X = {p2.X}, Y = {p2.Y}, Z = {p2.Z}");
         Console.WriteLine($"Расстояние между точками в 3D пространстве = {Math.Round(D, 2)}");
         }
-        StereoDistance();
-         
-        
-        
+        static void Cube()
+        {
+            {
+            Console.WriteLine("Введите число больше 1: ");
+            string a = new(Console.ReadLine());
+            int x;
+            if (int.TryParse(a, out x) && x > 1)
+            {   
+                Console.WriteLine("Кубы: ");
+                for (int i = 1; i < (x + 1); i++)
+                {
+                    Console.Write(i * i * i + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели что-то не то!");
+            }
+        }
 
-        
-    
-        
-        
+        }
+        static bool Exit()
+        {
+            Console.WriteLine("Хотите выйти?");
+            Console.WriteLine("Введите Y, чтобы выйти");
+            Console.WriteLine("Любая другая клавиша, чтобы остаться");
+            if (Console.ReadLine().ToLower() == "y")
+            {
+                return false;  
+            }
+            else
+            {
+                return true;
+            }
+        }
+        static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Выберите задачу: ");
+            Console.WriteLine("1. Задача 19");
+            Console.WriteLine("2. Задача 21");
+            Console.WriteLine("3. Задача 23");
+            Console.WriteLine("Для выхода нажмите Q");
+            
+            switch (Console.ReadLine().ToLower())
+            {
+                case "1":
+                FiveDigitPalindrome();
+                Spaces();
+                return Exit();
 
-    }
-    
+                case "2":
+                StereoDistance();
+                Spaces();
+                return Exit();
+
+                case "3":
+                Cube();
+                Spaces();
+                return Exit();
+
+                case "q":
+                return false;
+
+                default:
+                return true;
+            }
+        }
+        bool ShowMenu = true;
+        while (ShowMenu)
+        {
+            ShowMenu = MainMenu();
+        }       
+    }   
 }
