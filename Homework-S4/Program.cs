7841af9e840080826b547f423201d2a6be7c3024
arseny <arseniy.trofimov1993@gmail.com>
2022-09-13 14:50:59 +0300

    Added MakeAndPrintArray

--- Homework-S4/Program.cs
+++ Homework-S4/Program.cs
@@ -47,6 +47,22 @@
                 Console.WriteLine("Вы ввели что-то не то!");
             }
         }
+        static void MakeAndPrintArray()
+        {
+            int[] Array = new int[8];
+            Console.WriteLine("Введите 8 чисел, каждое на отдельной строке: ");
+            for (int i = 0; i < 8; i++)
+            {
+                Array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Список: ");
+            for (int j = 0; j < 8; j++)
+            {
+                Console.Write(Array[j] + " ");
+            }
+        }
         
-    }
+    }   
+
 }
