07cdaf85bd6a790b6b6fc1037bc5ce8428ff7997
arseny <arseniy.trofimov1993@gmail.com>
2022-09-15 11:45:13 +0300

    Created HowManyEvens

---
+++ Homework_S5/Program.cs
@@ -0,0 +1,18 @@
+﻿static void HowManyEvens()
+{   const int Size = 8;
+    int EvenCounter = 0;
+    int[] ThreeDigitNums = new int[Size];
+    for (int i = 0; i < Size; i++)
+    {
+        ThreeDigitNums[i] = new Random().Next(100, 1000);
+        if (ThreeDigitNums[i] % 2 == 0)
+        {
+            EvenCounter += 1;
+        }
+    }
+    Console.WriteLine($"Количество четных трехзначных чисел в массиве из 8 случайных трехзначных чисел: {EvenCounter}");
+}
+static void UnevenIndexSum()
+{
+    
+}
