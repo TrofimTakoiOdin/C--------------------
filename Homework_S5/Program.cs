d55d5b1965e82dfe667c2649c5e3dc3c8bceed13
arseny <arseniy.trofimov1993@gmail.com>
2022-09-16 13:16:31 +0300

    Added colours

--- Homework_S5/Program.cs
+++ Homework_S5/Program.cs
@@ -1,4 +1,4 @@
-﻿static void HowManyEvens()
+﻿static void HowManyEvens() // Задача 34
 {   const int Size = 8;
     int EvenCounter = 0;
     int[] ThreeDigitNums = new int[Size];
@@ -10,15 +10,21 @@
             EvenCounter += 1;
         }
     }
+    Console.WriteLine("Задан массив заполненный случайными положительными трёхзначными числами"); 
+    Console.WriteLine("Программа показывает количество чётных чисел в массиве.");
+    Console.WriteLine();
+    Console.BackgroundColor = ConsoleColor.Black; 
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
     Console.WriteLine($"Количество четных трехзначных чисел в массиве из 8 случайных трехзначных чисел: {EvenCounter}");
 }
-static void UnevenIndexSum()
+static void UnevenIndexSum() // Задача 36
 {
     const int Size = 10;
     Random rnd = new Random();
     int UnevenPosCounter = 0;
     int [] RandomNumbers = new int[Size];
     int i;
+    Console.WriteLine("Задан одномерный массив, заполненный случайными числами: ");
     Console.WriteLine("Массив случайных чисел: ");
     for (i = 0; i < Size; i++)
     {
@@ -30,21 +36,26 @@ static void UnevenIndexSum()
     {
         UnevenPosCounter += RandomNumbers[i];
     }
+    Console.BackgroundColor = ConsoleColor.Black; 
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    
     Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {UnevenPosCounter}");
+    Console.ResetColor();
 }
-static void MaxMinDifference()
+static void MaxMinDifference() // Задача 38
 {
     const int Size = 15;
     Random rnd = new Random();
     double [] DoubleArray = new double[Size];
     int i;
-    Console.BackgroundColor = ConsoleColor.White; 
-    Console.ForegroundColor = ConsoleColor.Red; 
+    Console.BackgroundColor = ConsoleColor.Black; 
+    Console.ForegroundColor = ConsoleColor.DarkGreen; 
     Console.WriteLine("Массив вещественных чисел: ");
+    Console.WriteLine();
 
     for (i = 0; i < Size; i++)
     {
-        DoubleArray[i] = rnd.Next(-100, 101);
+        DoubleArray[i] = rnd.Next(-100, 101) + rnd.NextDouble();
         Console.Write(DoubleArray[i] + " ");
     }
     Console.ResetColor();
@@ -54,15 +65,18 @@ static void MaxMinDifference()
     var Difference = Max - Min;
  
     Console.WriteLine();
-    Console.BackgroundColor = ConsoleColor.White; 
-    Console.ForegroundColor = ConsoleColor.DarkBlue; 
+    Console.BackgroundColor = ConsoleColor.Black; 
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine(); 
     Console.WriteLine($"Максимальное число в массиве: {Max}");
-    Console.WriteLine();
     Console.WriteLine($"Минимальное число в массиве: {Min}");
     Console.ResetColor();
     Console.WriteLine();
+    Console.BackgroundColor = ConsoleColor.Black; 
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
     Console.WriteLine("Искомая разность: ");
     Console.WriteLine($"{Max} — {Min} = {Difference}");
+    Console.ResetColor();
 }
 static int MinIndex(double[] Array)
 {
@@ -88,4 +102,64 @@ static int MaxIndex(double[] Array)
     }
     return MaxInd;
 }
-MaxMinDifference();
+static void Spaces()
+{
+    Console.WriteLine("=================");
+}
+static bool Exit()
+{
+    Console.WriteLine("Хотите выйти?");
+    Console.WriteLine("Введите Y, чтобы выйти");
+    Console.WriteLine("Любая другая клавиша, чтобы остаться");
+    if (Console.ReadLine().ToLower() == "y")
+    {
+        return false;  
+    }
+    else
+    {
+        return true;
+    }
+}
+static bool MainMenu()
+{
+    Console.Clear();
+    Console.BackgroundColor = ConsoleColor.Black; 
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine("Выберите задачу: ");
+    Console.WriteLine("1. Задача 34");
+    Console.WriteLine("2. Задача 36");
+    Console.WriteLine("3. Задача 38");
+    Console.WriteLine("Для выхода нажмите Q");
+    Console.ResetColor();
+            
+    switch (Console.ReadLine().ToLower())
+    {
+        case "1":
+        HowManyEvens();
+        Spaces();
+        return Exit();
+
+        case "2":
+        UnevenIndexSum();
+        Spaces();
+        return Exit();
+
+        case "3":
+        MaxMinDifference();
+        Spaces();
+        return Exit();
+
+        case "q":
+        return false;
+
+        default:
+        return true;
+    }
+}
+
+bool ShowMenu = true;
+while (ShowMenu)
+{
+    ShowMenu = MainMenu();
+}       
+  
