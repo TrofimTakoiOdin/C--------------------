4f8a27d80ab18b634c6d3e9b5ddabc2fdd5eee4a
arseny <arseniy.trofimov1993@gmail.com>
2022-09-19 15:23:17 +0300

    Solved pr 41

---
+++ Homework-S6/Program.cs
@@ -0,0 +1,18 @@
+﻿void CountNumsGreaterZero()
+{
+    Console.WriteLine("Сколько чисел будем вводить? (укажите число)");
+    int Size = Convert.ToInt32(Console.ReadLine());
+    double [] Array = new double[Size];
+    int Counter = 0;
+    Console.WriteLine("Вводим числа по одному, каждое на новой строке!");
+    for (int i = 0; i < Size; i++)
+    {
+        Array[i] = Convert.ToDouble(Console.ReadLine());
+        if (Array[i] > 0)
+        {
+            Counter++;
+        }
+    }
+    Console.WriteLine($"Количество чисел больше нуля: {Counter}");
+}
+CountNumsGreaterZero();
