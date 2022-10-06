8788d234476519587fec4fcf72a7beb47083931f
arseny <arseniy.trofimov1993@gmail.com>
2022-10-06 17:47:23 +0300

    WIP

---
+++ Homework-S9/Program.cs
@@ -0,0 +1,52 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+    }    
+    string[] EnterAndSplitString()
+    {
+        Console.Write("Введите числа через пробел, запятую или / : ");
+        return Console.ReadLine()!.Split(' ', ',', '/');
+    }
+    int[] ParseInput(string[] nums)
+    {
+        int[] integerNums = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            integerNums[i] = int.Parse(nums[i])!;
+        }
+        return integerNums;
+    }
+    void FromNtoOne(int num)
+    {
+        if (num > 0)
+        {
+            Console.Write($"{num} ");
+            FromNtoOne(num - 1);
+        }
+        else
+        {
+            Console.WriteLine();
+            return;
+        }
+    }
+    int BetweenNumsSum(int a, int b)
+    {
+        int total = 0;
+        if (a == b)
+        {
+            return a;
+        }
+        total += b;
+        return BetweenNumsSum(a, b - 1) + total;
+
+    }
+    int Akkerman(int x, int y)
+    {
+        if (x == 0) return y + 1;
+        else if (x > 0 && y == 0) return Akkerman(x - 1, 1);
+        return Akkerman(x - 1, Akkerman(x, y - 1));
+    }
+    
+}
